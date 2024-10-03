47a95d9acb25b2281e609b078f6f6159561817da
Emanuel López <emanuel.lopez@globant.com>
2024-10-03 07:20:03 -0300

    Refactoriza arquitectura de la aplicación

---
+++ clase08/counter_test/lib/src/presentation/counter_screen.dart
@@ -0,0 +1,46 @@
+import 'package:counter_test/src/domain/domain.dart';
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  final _counter = Counter();
+
+  void _incrementCounter() {
+    setState(() {
+      _counter.increment();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Flutter Demo Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '${_counter.value}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
